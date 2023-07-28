6b6ec3f326fefc3024f68c2127327bcfd6fb4df0
Subham <subhampalit.workspace@gmail.com>
2023-07-28 20:46:13 +0530

    Version update 0.1.0

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import About from './components/About';
 import Navbar from './components/Navbar'
 import Textform from './components/Textform'
 import Alerts from './components/Alerts';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 
@@ -44,17 +44,17 @@ function App() {
   }
   return (
     <>
-      {/* <Router> */}
+      <Router>
         <Navbar title='TextUtils' abouttext='About Us' mode={mode} togglemode={togglemode} />
         <Alerts alert={alert} />
         <div className="container my-3">
-          {/* <Routes>
-            <Route exact path="/about" element={<About mode={mode}/>}/> */}
-            {/* <Route exact path="/" element={<Textform showAlert={showAlert} heading='Enter the text to analyze below' mode={mode} />}/> */}
-            <Textform showAlert={showAlert} heading='Try TextUtils - Word Counter, Character Counter' mode={mode} />
-          {/* </Routes> */}
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode}/>}/>
+            <Route exact path="/" element={<Textform showAlert={showAlert} heading='Enter the text to analyze below' mode={mode} />}/>
+            {/* <Textform showAlert={showAlert} heading='Try TextUtils - Word Counter, Character Counter' mode={mode} /> */}
+          </Routes>
         </div>
-      {/* </Router> */}
+      </Router>
     </>
 
   );
